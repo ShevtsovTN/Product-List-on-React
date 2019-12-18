9137af6c28cc3b615e553c042a365ab61485c77c
ShevtsovTN <51271499+ShevtsovTN@users.noreply.github.com>
2019-12-18 18:24:13 +0100

    Add files via upload

---
+++ src/shop/detail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Detail extends React.Component {
+
+    render() {
+        const {phone, className} = this.props;
+
+        return (
+          <div className={className}>
+              <h3>{phone.name}</h3>
+              <small>{phone.id}</small>
+              <img src={phone.imageUrl} alt={phone.name}/>
+              <p>{phone.snippet}</p>
+          </div>
+        );
+    }
+}
